a513c80a576748457850569421e4fbe8ccdec556
sommanek <sommaneknimesh@gmail.com>
2014-09-07 12:23:50 +0530

    Appointment Module...

---
+++ EHRProject/src/main/java/com/EHR/DAOImpl/AppointmentDAOImpl.java
@@ -0,0 +1,7 @@
+package com.EHR.DAOImpl;
+
+import com.EHR.DAO.AppointmentDAO;
+
+public class AppointmentDAOImpl implements AppointmentDAO {
+
+}
---
+++ EHRProject/src/main/java/com/EHR/ServiceImpl/AppointmentServiceImpl.java
@@ -0,0 +1,7 @@
+package com.EHR.ServiceImpl;
+
+import com.EHR.Service.AppointmentService;
+
+public class AppointmentServiceImpl implements AppointmentService {
+
+}
