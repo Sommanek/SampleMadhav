7b4ffdd7e48066846ffcc98f650c397844b3bc7b
sommanek <sommaneknimesh@gmail.com>
2014-09-07 16:03:02 +0530

    Test

--- EHRProject/src/main/java/com/EHR/Controller/AppointmentController.java
+++ EHRProject/src/main/java/com/EHR/Controller/AppointmentController.java
@@ -1,14 +1,20 @@
 package com.EHR.Controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.EHR.bean.Patient;
 
 @Controller
 @RequestMapping("/Appointment")
 public class AppointmentController {
 	@RequestMapping("/AddAppointment")
 	public String addAppointmentHandler(ModelMap modelMap) {
+		Patient patient = new Patient();
+		System.out.println("123123::"+patient.getPatientId());
 		return "AddAppointment";
 	} 
+	
+	
 }
